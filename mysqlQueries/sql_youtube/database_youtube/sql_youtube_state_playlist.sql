28d9e2762840f7ce34683c653eaf6411b2686051
roche-tan <rochelyn.rallestan@gmail.com>
2024-01-08 10:47:10 +0100

    added spotify and youtube databases and diagram

--- mysqlQueries/sql_youtube/database_youtube/sql_youtube_state_playlist.sql
+++ mysqlQueries/sql_youtube/database_youtube/sql_youtube_state_playlist.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `sql_youtube` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sql_youtube`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: sql_youtube
@@ -48,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-21  9:48:51
+-- Dump completed on 2024-01-08 10:31:34
